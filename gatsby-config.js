0036cc0a50fb375ffabd1027b43c908c7551fb5a
Jouler Jose Talaigua Hoyos <jouler.talaigua@pragma.com.co>
2022-10-19 14:03:29 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `JoulerFy`,
+    description: `JoulerFy is a Gatsby starter for creating a blog with a minimalistic design.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
